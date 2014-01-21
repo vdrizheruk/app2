8b1b75c5cce12b5b198bc8823782c61564023205
Vladimir Drizheruk <vdrizheruk@magecore.com>
2014-01-21 11:40:52 +0000

    init

---
+++ src/app2/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dvv
+ * Date: 1/21/14
+ * Time: 1:33 PM
+ */
+namespace app2;
+
+class Test
+{
+    public static function hello()
+    {
+        return 'test hello';
+    }
+
+} 
